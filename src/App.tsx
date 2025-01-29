7eb96ff345779296ec3844357f36bdf7eddec141
rahem <reza.rahem224@gmail.com>
2025-01-29 20:20:26 +0330

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from './components/button';
+
 function App() {
- return <div className='bg-amber-300'>react comp</div>;
+ return (
+  <div className='m-5'>
+   <Button>test</Button>
+  </div>
+ );
 }
 
 export default App;
